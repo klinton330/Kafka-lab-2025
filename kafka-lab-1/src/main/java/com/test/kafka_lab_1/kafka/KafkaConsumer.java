4bc1bf978ae17562f941c586281d358bcddcd483
HOME <klintonece@gmail.com>
2025-10-20 22:49:36 +0530

    lab 1

---
+++ kafka-lab-1/src/main/java/com/test/kafka_lab_1/kafka/KafkaConsumer.java
@@ -0,0 +1,16 @@
+package com.test.kafka_lab_1.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+
+    private  static final Logger logger= LoggerFactory.getLogger(KafkaConsumer.class);
+    @KafkaListener(topics = "chennai",groupId = "myGroup")
+    public void consume(String message){
+        logger.info(String.format("Message received->%s",message));
+    }
+}
---
+++ kafka-lab-2/src/main/java/com/test/kafka_lab_1/KafkaLab1Application.java
@@ -0,0 +1,13 @@
+package com.test.kafka_lab_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaLab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaLab1Application.class, args);
+	}
+
+}
